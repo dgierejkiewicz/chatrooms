8945fd8c30df0c1450e63785fccf6c40c5cb19c3
Dariusz <dariusz.gierejkiewicz@gmail.com>
2017-05-15 20:02:54 +0200

    add chat_server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // node modules
-var http 	= require('http');
-var fs 		= require('fs');
-var path 	= require('path');
-var mime 	= require('mime');
-var cache 	= {};
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 
 /********************************* server **********************************/
 var server = http.createServer(function (request, response) {
@@ -30,11 +33,11 @@ server.listen(3000, function () {
  * @return {[type]}
  */
 function send404(response) {
-    response.writeHead(404, {
-        'Content-Type': 'text/plain'
-    });
-    response.write('Błąd 404: plik nie został znaleziony');
-    response.end();
+	response.writeHead(404, {
+		'Content-Type': 'text/plain'
+	});
+	response.write('Błąd 404: plik nie został znaleziony');
+	response.end();
 }
 
 /**
@@ -45,10 +48,10 @@ function send404(response) {
  * @return {[type]}              [description]
  */
 function sendFile(response, filePath, fileContents) {
-    response.writeHead(200, {
-        'Content-Type': mime.lookup(path.basename(filePath))
-    });
-    response.end(fileContents);
+	response.writeHead(200, {
+		'Content-Type': mime.lookup(path.basename(filePath))
+	});
+	response.end(fileContents);
 }
 
 /**
@@ -59,23 +62,23 @@ function sendFile(response, filePath, fileContents) {
  * @return {[type]}          [description]
  */
 function serveStatic(response, cache, absPath) {
-    if (cache[absPath]) {
-    	sendFile(response, absPath, cache[absPath]);
-    } else {
-    	fs.exists(absPath, function (exists) {
-    		if (exists) {
-    			fs.readFile(absPath, function (err, data) {
-    				if (err) {
-    					send404(response);
-    				} else {
-    					cache[absPath] = data;
-    					sendFile(response, absPath, data);
-    				}
-    			});
-    		} else {
-    			send404(response);
-    		}
-    	});
-    }
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function (exists) {
+			if (exists) {
+				fs.readFile(absPath, function (err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
 
 }
